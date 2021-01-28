2a82f43bc9926a761a7e486f4a6b27eefc499b23
John Anthony <jmanthony.fullsail@gmail.com>
2021-01-28 13:55:26 -0600

    added h1 tags and set text variables

--- AwesomeJavaScript.js
+++ AwesomeJavaScript.js
@@ -1,1 +1,19 @@
 console.log('I have JS');
+
+// always the first things you do in jQuery
+$(document).ready(onReady);
+
+function onReady() {
+  console.log('jQuery Activated');
+
+  const h1 = $('h1');
+  console.log('h1', h1);
+
+  const h2 = $('h2');
+  console.log('h2', h2);
+
+  $('h2').addClass('froggy');
+
+  const h1Text = $('h1').text();
+  console.log('h1Text is ', h1Text);
+}

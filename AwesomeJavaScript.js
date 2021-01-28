a25e516532c1a4bde0b38534be2a236ffb1bf70a
John Anthony <jmanthony.fullsail@gmail.com>
2021-01-28 16:23:00 -0600

    using the event method and how to call it to make sure your form tag does not refresh with the dom

--- AwesomeJavaScript.js
+++ AwesomeJavaScript.js
@@ -19,4 +19,40 @@ function onReady() {
 
   const louderText = h1Text.toUpperCase() + '!!!!';
   $('h1').text(louderText);
+  // when this button is clicked, delete function runs
+  //$('#submitButton').on('click', deleteMe);
+  // more correct term for the code above but more specific and fail proof
+  $(document).on('click', '.deleteButton', deleteMe);
+  // something to happen when someone clicks a button
+  // called event handlers or click events
+  $('#submitButton').on('click', whenIaddAharmonica);
+}
+function deleteMe() {
+  // this method
+  // using this method is how you delete just this item being created
+  // this is item is referred to the list item created inside of the ul element class
+  let thisThing = $(this);
+  console.log('this is the delete', thisThing);
+
+  $(this).parent().remove();
+  //console.log('delete me');
+  //$('.harmonica-item').remove();
+}
+function whenIaddAharmonica(event) {
+  // event for preventing the form from reloading the page
+  event.preventDefault();
+
+  //stuff to do when call function
+  console.log('clicked the button');
+
+  // grab the data
+  let brand = $('#brand').val();
+  console.log('brand is ', brand);
+
+  $('#harmonica').append(`
+  <li class="harmonica-item">
+  Brand is: ${brand}
+  <button class="deleteButton">Delete Me</button>
+  </li>
+  `);
 }
